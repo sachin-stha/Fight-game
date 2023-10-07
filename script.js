d92bd35188562a07b30ccac23f265ae67e0fbdbd
sachin-stha <sachinshrestha7055@gmail.com>
2023-10-07 17:36:56 +0545

    project setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#myCanvas");
+const ctx = canvas.getContext("2d");
+
+let canvasSize = { width: 1000, height: 600 };
+
+canvas.width = canvasSize.width;
+canvas.height = canvasSize.height;
